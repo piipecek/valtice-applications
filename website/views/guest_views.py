6bb07c2942a74921ae5302b4f2cd1f826ab1f0b6
Josef Lát <josef.latj@gmail.com>
2025-01-28 08:39:29 +0100

    cz en prihlaska

--- website/views/guest_views.py
+++ website/views/guest_views.py
@@ -12,11 +12,15 @@ guest_views = Blueprint("guest_views",__name__)
 def cz_dashboard():
     return render_template("guest/cz_dashboard.html", roles=get_roles(current_user), faze = get_faze_for_dashboard(), date_zacatku = get_date_zacatku_for_dashboard(), time_zacatku = get_time_zacatku_for_dashboard() )
 
-@guest_views.route("/prihlaska")
-def prihlaska():
-    return render_template("guest/prihlaska.html")
-
 @guest_views.route("/en/")
 @guest_views.route("/en/dashboard")
 def en_dashboard():
     return render_template("guest/en_dashboard.html", roles=get_roles(current_user), faze = get_faze_for_dashboard(), date_zacatku = get_date_zacatku_for_dashboard(), time_zacatku = get_time_zacatku_for_dashboard() )
+
+@guest_views.route("/prihlaska")
+def cz_prihlaska():
+    return render_template("guest/cz_prihlaska.html")
+
+@guest_views.route("/en_prihlaska")
+def en_prihlaska():
+    return render_template("guest/en_prihlaska.html")
