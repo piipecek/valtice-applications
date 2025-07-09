892ef892f06f62d287844e658da39f513fcc9ae7
Josef Lát <josef.latj@gmail.com>
2025-07-09 18:25:18 +0200

    ruzicka

--- website/views/guest_views.py
+++ website/views/guest_views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, flash
 from flask_login import current_user
 from website.helpers.get_roles import get_roles
+import datetime
 
 guest_views = Blueprint("guest_views",__name__)
 
@@ -25,9 +26,9 @@ def en_prihlaska():
 
 @guest_views.route("/informace")
 def cz_info():
-    return render_template("guest/cz_info.html", roles=get_roles(current_user))
+    return render_template("guest/cz_info.html", roles=get_roles(current_user), year=datetime.datetime.now().year)
 
 @guest_views.route("/en_informace")
 def en_info():
-    return render_template("guest/en_info.html", roles=get_roles(current_user))
+    return render_template("guest/en_info.html", roles=get_roles(current_user), year=datetime.datetime.now().year)
 
